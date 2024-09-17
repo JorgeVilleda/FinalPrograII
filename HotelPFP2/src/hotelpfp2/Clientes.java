6812569aed1eff712f661e5c2d789d0872deae72
Jorge <Jorge@SJ00IFT08.COOSAJO.MICOOPE.local>
2024-09-17 17:10:32 -0600

    Se agregaron los archivos para las clases de java principales, además de las administrativas y para la conexión de la base de datos.

---
+++ HotelPFP2/src/hotelpfp2/Clientes.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotelpfp2;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Clientes {
+    
+}
